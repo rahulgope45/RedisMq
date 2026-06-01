db912719832e9b7309aa400af5682b6fc40a527c
rahulgope45 <rahuleile456@gmail.com>
2026-06-01 19:32:01 +0530

    Added a test route for lPush,lRange and lPop

--- src/server.ts
+++ src/server.ts
@@ -12,12 +12,55 @@ app.get("/", (req: Request, res: Response) => {
 // ============ Testing Redis ========
 
 app.get('/testRedis',async(req:Request,res:Response)=>{
-    await redisClient.set('name','Rahul')
+    await redisClient.set('name','Rahul',{
+        EX: 60
+    })
     const value = await redisClient.get('name');
 
     res.json({
         value
     })
+})
+console.log("Loading add-job route...");
+
+//============ Testing Qeueu  using lPush, lRange and RPOP ==========
+
+app.get('/add-job',async(req:Request,res:Response)=>{
+  const job ={
+    to: "test@gmail.com",
+    subject: "hello",
+    createdAt: Date.now()
+  };    
+
+  await redisClient.lPush(
+    'emailQueue',
+    JSON.stringify(job)
+  );
+
+  res.send('Job Added')
+})
+
+app.get('/jobs',async(req:Request,res:Response)=>{
+    const jobs  = await redisClient.lRange(
+        'emailQueue',
+        0,
+        -1
+    );
+    res.send(jobs)
+});
+
+app.get('/process-job', async(req:Request,res:Response)=>{
+    const jobs = await redisClient.lPop(
+        'emailQueue'
+    );
+
+    if(!jobs){
+        return res.send('No jobs found');
+    }
+
+    console.log('Processing',JSON.parse(jobs));
+
+    res.send('Job Processed')
 })
 
 async function startServer() {
