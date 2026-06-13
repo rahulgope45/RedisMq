0cadaf473dca7209b7eb784e061676a9705b59c9
rahulgope45 <rahuleile456@gmail.com>
2026-06-13 12:53:41 +0530

    Bullmq file changed

--- src/config/bullmq.ts
+++ src/config/bullmq.ts
@@ -1,6 +1,8 @@
+const redisUrl = new URL(process.env.REDIS_URL || 'redis://redis:6379');
+
 export const redisConnection = {
-    host: process.env.REDIS_HOST || 'redis',
-    port: Number(process.env.REDIS_PORT) || 6379,
-    password: process.env.REDIS_PASSWORD,
-    tls: process.env.REDIS_HOST?.includes('upstash') ? {} : undefined
+    host: redisUrl.hostname,
+    port: Number(redisUrl.port),
+    password: redisUrl.password || undefined,
+    tls: redisUrl.protocol === 'rediss:' ? {} : undefined
 }
